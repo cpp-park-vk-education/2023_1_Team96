7d384781f14e0fbfd2071bcc92be7ba110a7bd20
HallowAz <amigo_honor@mail.ru>
2023-05-04 18:28:31 +0300

    add without hpp

---
+++ test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <memory>
+#include <boost/system/error_code.hpp>
+
+using namespace boost::asio;
+using namespace boost::placeholders;
+
+int main()
+{
+  return 0;
+}
