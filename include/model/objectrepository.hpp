746e5583dca0f30f53ba2cd3a85c6e4f4e25987f
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-05-13 03:06:51 +0300

    Добавил репозиторий объектов

--- include/model/objectrepository.hpp
+++ include/model/objectrepository.hpp
@@ -1,131 +1,40 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
 #include <memory>
 #include <vector>
-#include <SFML/Graphics.hpp>
-#include "graphics/graphics.hpp"
 
 #include "gameobject.hpp"
+#include "graphics/graphics.hpp"
 
-using std::vector;
-using std::unique_ptr;
+struct IObjectRepository {
+    using std::shared_ptr;
 
-enum UnitType
-{
-    B,
-    K,
-};
-
-class IObjectRepository {
-public:
-    virtual void Add(std::unique_ptr<GameObject> obj) = 0;
-    virtual std::shared_ptr<GameObject> GetByCords(sf::Vector2u cords) = 0;
-    virtual std::shared_ptr<GameObject> GetByPlayer(Player player) = 0;
-    virtual void Delete(std::shared_ptr<GameObject> obj) = 0;
-    virtual void DeleteByCords(sf::Vector2u cords) = 0;
-    virtual void draw() = 0;
-    virtual void setCurrent(sf::Vector2i pos) = 0;
-    virtual void resetCurrent() = 0;
-    virtual sf::Vector2i getCurrent() = 0;
-    virtual bool createUnit(UnitType type, std::unique_ptr<IModel> model) = 0;
-    virtual void deleteUnit(sf::Vector2u pos) = 0;
+    virtual bool Add(shared_ptr<GameObject> obj) = 0;
+    virtual shared_ptr<GameObject> GetByCords(sf::Vector2u cords) = 0;
+    virtual shared_ptr<GameObject> GetByPlayer(Player player) = 0;
+    virtual bool Delete(shared_ptr<GameObject> obj) = 0;
+    virtual bool DeleteByCords(sf::Vector2u cords) = 0;
 
     virtual ~IObjectRepository() {}
 };
 
-class Field : public IObjectRepository
-{
-private:
-    vector<std::shared_ptr<GameObject>> objects;
-    unsigned int h, w;
-    std::unique_ptr<IFieldModel> model;
-    sf::Vector2i current = {-1,-1};
+class ObjectRepository : public IObjectRepository {
+    using std::vector, std::shared_ptr;
 
-public:
-    Field(sf::Vector2u size, std::unique_ptr<IFieldModel>&& f_model) : objects(size.x * size.y), 
-                                                     model(std::move(f_model))
-    {
-        h = size.x; w = size.y;
+   private:
+    vector<vector<shared_ptr<GameObject>>> objects_;
+    unsigned int h_, w_;
+
+   public:
+    ObjectRepository(int field_height, int field_width)
+        : objects_(field_height), h_(field_height), w_(field_width) {
+        for (int i = 0; i < field_height; ++i) objects_[i].resize(field_width);
     }
 
-    bool createUnit(UnitType type, std::unique_ptr<IModel> model)
-    {
-        sf::Vector2i pos = current;
-        int index = pos.y * w + pos.x;
-        std::cout << pos.x  << std::endl;
-        std::cout << pos.y  << std::endl;
-        std::cout << index << std::endl;
-
-        if (index >= objects.size()) return false;
-
-        if (pos.x >= 7) return false;
-
-        if (objects[index] != nullptr) return false;
-
-
-        switch (type)
-        {
-        case B:
-        
-            objects[index] = std::make_shared<GameObject>(std::move(model));
-            objects[index]->AddAction(ActionType::MOVE, std::move(std::make_unique<MoveAction>(*(objects[index]), sf::Vector2u{pos.x, pos.y},1)));
-
-            std::cout << "created!" << std::endl;
-            
-            return true;
-
-        case K:
-            
-            break;
-        
-        }
-    };
-
-    void deleteUnit(sf::Vector2u pos) override
-    {
-        objects[pos.y*w + pos.x] = nullptr;
-    };
-
-    void Add(std::unique_ptr<GameObject> obj)
-    {
-
-    };
-
-    std::shared_ptr<GameObject> GetByCords(sf::Vector2u cords)
-    {
-
-    };
-
-    void setCurrent(sf::Vector2i pos) override 
-    {
-        current.x = pos.x;
-        current.y = pos.y;
-        model->setCurrent(current);
-        std::cout << pos.x << std::endl;
-    };
-
-    void resetCurrent() override
-    {
-        sf::Vector2i res = current;
-        current.x = -1;
-        current.y = -1;
-        model->resetCurrent();
-    };
-
-    sf::Vector2i getCurrent() override
-    {
-        return current;
-    };
-
-    std::shared_ptr<GameObject> GetByPlayer(Player player){};
-    void Delete(std::shared_ptr<GameObject> obj){};
-    void DeleteByCords(sf::Vector2u cords){};
-    void draw() 
-    {
-        model->render();
-        for (std::shared_ptr<GameObject> object : objects)
-        {
-            if (object) object->draw();
-        }
-    };
+    bool Add(shared_ptr<GameObject> obj) override;
+    shared_ptr<GameObject> GetByCords(sf::Vector2u cords) override;
+    shared_ptr<GameObject> GetByPlayer(Player player) override;
+    bool Delete(shared_ptr<GameObject> obj) override;
+    bool DeleteByCords(sf::Vector2u cords) override;
 };
---
+++ src/model/objectrepository.cpp
@@ -0,0 +1,47 @@
+#include "model/objectrepository.hpp"
+
+bool ObjectRepository::Add(shared_ptr<GameObject> obj) {
+    int x_cord = obj->Pos().x;
+    int y_cord = obj->Pos().y;
+
+    if (x_cord >= w_ || y_cord >= h_) return false;
+
+    objects_[y_cord][x_cord] = obj;
+    return true;
+}
+
+shared_ptr<GameObject> ObjectRepository::GetByCords(sf::Vector2u cords) {
+    if (cords.x >= w_ || cords.y >= h_)
+        return nullptr;
+    else
+        return objects_[cords.y][cords.x];
+}
+
+shared_ptr<GameObject> ObjectRepository::GetByPlayer(Player player) {
+    for (int y = 0; y < h_; ++y) {
+        for (int x = 0; x < w_; ++x) {
+            if (*objects_[y][x].GetPlayer() == player) return objects_[y][x];
+        }
+    }
+
+    return nullptr;
+}
+
+bool ObjectRepository::Delete(shared_ptr<GameObject> obj) {
+    int x_cord = obj->Pos().x;
+    int y_cord = obj->Pos().y;
+
+    if (x_cord >= w_ || y_cord >= h_) return false;
+
+    objects_[y_cord][x_cord] = nullptr;
+    return true;
+}
+
+bool ObjectRepository::DeleteByCords(sf::Vector2u cords) {
+    if (cords.x >= w_ || cords.y >= h_) {
+        return false;
+    } else {
+        objects_[cords.y][cords.x] = nullptr;
+        return true;
+    }
+}
