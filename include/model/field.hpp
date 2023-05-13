17a7658b2489779ba7bb9e1029477d5a7ef03806
Maxim Davydov <norm.pochta.maxa@gmail.com>
2023-05-13 03:07:17 +0300

    Добавил игровое поле

---
+++ include/model/field.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "objectrepository.hpp"
+
+class Field {
+   private:
+    IObjectRepository repo_;
+    std::unique_ptr<IFieldModel> model_;
+    sf::Vector2i current_ = {-1, -1};
+
+   public:
+    Field(IObjectRepository repository, std::unique_ptr<IFieldModel> f_model)
+        : repo_(repository), model(std::move(f_model)) {}
+
+    bool CreateUnit(UnitType type, std::unique_ptr<IModel> model);
+    bool DeleteUnit(sf::Vector2u pos);
+
+    void SetCurrent(sf::Vector2i pos);
+    void ResetCurrent();
+    sf::Vector2i GetCurrent();
+
+    void Draw();
+};
